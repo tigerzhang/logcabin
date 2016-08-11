a3546e39c32f93caa8d93f8f5ca50e412f0caafa
Zhang Hu <zhanghu@yunba.io>
2016-08-11 13:11:58 +0800

    add deps

--- make.sh
+++ make.sh
@@ -1,10 +1,12 @@
+#!/usr/bin/env bash
 git submodule add https://github.com/facebook/rocksdb rocksdb
 git submodule update --init
 cd rocksdb/
 make static_lib
 sudo make install
 cd -
-deps=("lz4" "jemalloc")
+deps=("lz4" "jemalloc" "crypto++" "protobuf" "snappy" "bz2")
 for i in "${deps[@]}" ; do
 	sudo apt-get install lib$i-dev
 done
+sudo apt-get install protobuf-compiler
